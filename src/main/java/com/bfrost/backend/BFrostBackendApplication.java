4bffeda7d2848f1bd417cefc893501200b110cfe
BillerPlay <abulyaa@gmail.com>
2026-06-30 14:50:11 +0400

    Initial commit

---
+++ src/main/java/com/bfrost/backend/BFrostBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bfrost.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BFrostBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BFrostBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bfrost/backend/BFrostBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfrost.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BFrostBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
