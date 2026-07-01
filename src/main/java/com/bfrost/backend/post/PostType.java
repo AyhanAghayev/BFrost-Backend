f76706d679d88f4bb373e378faeefd6994d168f7
gurban200OK <qurbankerem@gmail.com>
2026-07-02 00:41:34 +0400

    feat: add Post entity

---
+++ src/main/java/com/bfrost/backend/post/PostType.java
@@ -0,0 +1,7 @@
+package com.bfrost.backend.post;
+
+public enum PostType {
+    TEXT,
+    IMAGE,
+    LINK
+}
---
+++ src/main/java/com/bfrost/backend/post/TargetType.java
@@ -0,0 +1,6 @@
+package com.bfrost.backend.post;
+
+public enum TargetType {
+    USER_PAGE,
+    CLUB_PAGE
+}
